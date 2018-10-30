49f7eb8b44b6a8f6fc071174ef222262e96d938d
lishuyi <876317233@qq.com>
2018-10-30 17:27:25 +0800

    standby from 2018.10.30

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+' a demo '
+
+__author__ = 'Shuyi Li'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'lishuyi',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
